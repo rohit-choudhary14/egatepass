c62fbd799b936492ea3262d0bae9a5e0cba68bb3
Rohit choudhary <kartikjaat5765@gmail.com>
2025-12-19 14:03:58 +0530

    Final commit

--- urlConfig.php
+++ urlConfig.php
@@ -1,14 +1,15 @@
 <?php
+session_start();
 ob_start();
-ini_set('session.cookie_httponly', 1);
+
 date_default_timezone_set('Asia/Kolkata');
+ini_set('date.timezone', 'Asia/Kolkata');
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
+//error_reporting(0);
 header('X-Frame-Options: DENY');
 header("X-XSS-Protection: 1; mode=block");
+
+ini_set( 'session.cookie_httponly', 1 );
 
 $url = $_SERVER['REQUEST_URI'];
 $query_str = parse_url($url, PHP_URL_QUERY);
@@ -16,7 +17,7 @@ parse_str($query_str, $query_params);
 
 $folderName =basename(dirname(__FILE__));
 
-$base_url="http://localhost:".$_SERVER['SERVER_PORT']."/".$folderName."/";
+$base_url="https://hcraj.nic.in:".$_SERVER['SERVER_PORT']."/".$folderName."/";
 
 if(!empty($query_params))
 {
